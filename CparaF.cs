4fa713c4ee9ced749149be29e8290d924ec953a2
Luan Magno <81940672+LuanMagno@users.noreply.github.com>
2023-10-17 09:27:27 -0300

    Add files via upload

---
+++ CparaF.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double TempC;
+            double TempF;
+
+
+            Console.WriteLine("Digite a temperatura em C°: ");
+            TempC = double.Parse(Console.ReadLine());
+            TempF = TempC * (9.0/5) + 32;
+            Console.WriteLine("A temperatura em Fahreheit é : " + TempF);
+        }
+    }
+}
---
+++ IMC.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float TempC;
+            float TempF;
+            
+            
+            Console.WriteLine("Digite a temperatura em C°: ");
+            TempC = float.Parse(Console.ReadLine());
+            TempF = TempC * 1.8 + 32;
+            Console.WriteLine("A temperatura em Fahreheit é : " + TempF);
+        }
+    }
+}
