89b828b003b528dd87b6108f75f10e14c852228c
Luan Magno <81940672+LuanMagno@users.noreply.github.com>
2023-10-17 09:28:21 -0300

    Add files via upload

---
+++ RaioC.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double PerimetroC;
+            double AreaC;
+            double RaioC;
+
+
+            Console.WriteLine("Digite o raio do círculo: ");
+            RaioC = double.Parse(Console.ReadLine());
+            AreaC = Math.PI * Math.Pow(RaioC, 2);
+            PerimetroC = 2 * Math.PI * RaioC;
+            Console.WriteLine("A área do círculo é : " + AreaC);
+            Console.WriteLine("O perímetro do círculo é :" + PerimetroC);
+        }
+    }
+}
