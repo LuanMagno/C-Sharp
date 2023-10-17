64d047d6ee55085c8d1c9aad6ed3ea146c16b100
Luan Magno <81940672+LuanMagno@users.noreply.github.com>
2023-10-17 09:28:01 -0300

    Add files via upload

---
+++ FparaC.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FparaC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double TempF;
+            double TempC;
+            Console.WriteLine("Digite a temperatura em F°: ");
+            TempF = double.Parse(Console.ReadLine());
+            TempC = 5.0 * ((TempF - 32.0) / 9.0);
+            Console.WriteLine("A temperatura em C° é: " + TempC);
+        }
+    }
+}
---
+++ HipotenusaTriangulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TrianguloRetangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float Cateto1;
+            float Cateto2;
+            double Hipotenusa;
+            
+            Console.WriteLine("Insira o primeiro cateto do triângulo:");
+            Cateto1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Insira o segundo cateto do triângulo:");
+            Cateto2 = float.Parse(Console.ReadLine());
+            Hipotenusa = Math.Pow(Cateto1, 2) + Math.Pow(Cateto2, 2);
+            Hipotenusa = Math.Sqrt(Hipotenusa);
+            Console.WriteLine("A hipotenusa é: " + Hipotenusa);
+        }
+    }
+}
