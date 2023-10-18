9f6278e6cfc99bc28b0929dc79bad8e0ea9973b0
Luan Magno <81940672+LuanMagno@users.noreply.github.com>
2023-10-18 14:36:52 -0300

    Add files via upload

---
+++ Kw.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Killowatt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double SalarioM, Kilowatt, ValorKw, Vt;
+            Console.WriteLine("Escreva o valor do salário mínimo: ");
+            SalarioM = double.Parse(Console.ReadLine());
+            Console.WriteLine("Escreva a quantidade do kilowatt gasto: ");
+            Kilowatt = double.Parse(Console.ReadLine());
+            ValorKw = 0.001 * SalarioM;
+            Vt = ValorKw * Kilowatt;   
+            Console.WriteLine("O valor de cada Kw é R$" + ValorKw + ", e o valor total é de R$" + Vt);  
+        }
+    }
+}
