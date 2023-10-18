fe184427106166fb9476e576465cc89ea337025b
Luan Magno <81940672+LuanMagno@users.noreply.github.com>
2023-10-18 14:34:46 -0300

    Add files via upload

---
+++ DiasTrabalhados.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TrocaVariavel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int DiasT, ValorF;
+            Console.WriteLine("Quantos dias foram trabalhados: ");
+            DiasT = (int.Parse(Console.ReadLine()));
+            ValorF = (DiasT * 30);
+            ValorF = (ValorF * 92) / 100;
+            Console.WriteLine("O valor a ser recebido é R$" + ValorF);
+        }
+    }
+}
