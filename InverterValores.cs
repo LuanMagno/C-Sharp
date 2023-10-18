9b25ea25d5070cc97e295e130af6e342bfd092e1
Luan Magno <81940672+LuanMagno@users.noreply.github.com>
2023-10-18 14:36:33 -0300

    Add files via upload

---
+++ InverterValores.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EnergiaGasta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ValorInt,Centena,Dezena,Unidade;
+            Console.WriteLine("Insira um valor inteiro de 3 dígitos");
+            ValorInt = (int.Parse(Console.ReadLine()));
+            Unidade = ValorInt % 10;
+            Dezena = ((ValorInt % 100)-Unidade)/10;
+            Centena = ((ValorInt % 1000) - Unidade - Dezena)/100;
+            Console.WriteLine(Unidade+Dezena+Centena);
+        }
+    }
+}
