2dc8277d0fffbc606709c59a53056aae03237bb1
Luan Magno <81940672+LuanMagno@users.noreply.github.com>
2023-10-18 14:35:43 -0300

    Add files via upload

---
+++ SalarioFinal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SalarioBase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Salario,SalarioG,SalarioI;
+            Console.WriteLine("Insira o salário base: ");
+            Salario = double.Parse(Console.ReadLine());
+            SalarioG = Salario * 1.05;
+            SalarioI = Salario * 0.07;
+            Salario = SalarioG - SalarioI;
+            Console.WriteLine("O salário final será de R$" + Salario);
+            Console.WriteLine();
+        }
+    }
+}
