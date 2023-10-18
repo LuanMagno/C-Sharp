1c49afeaaaf1dbd6c03305866eae7f77b43f379a
Luan Magno <81940672+LuanMagno@users.noreply.github.com>
2023-10-18 14:36:15 -0300

    Add files via upload

---
+++ VotosValidos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Eleitores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Eleitores, brancos, nulos, validos;
+            Console.WriteLine("Insira a quantidade de eleitores no município: ");
+            Eleitores = (int.Parse(Console.ReadLine()));
+            Console.WriteLine("Quantos votaram branco: ");
+            brancos = (int.Parse(Console.ReadLine()));
+            Console.WriteLine("Quantos votaram nulo: ");
+            nulos = (int.Parse(Console.ReadLine()));
+            Console.WriteLine("Quantos votos são válidos: ");
+            validos = (int.Parse(Console.ReadLine()));
+            validos = (validos * 100) / Eleitores;
+            brancos = (brancos * 100) / validos;
+            nulos = (nulos * validos) / validos;
+            Console.WriteLine("O percentual de votos brancos é de " + brancos + ", de votos nulo é de " + nulos + ", e de votos válidos é de " + validos);
+        }
+    }
+}
