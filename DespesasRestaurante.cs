c8c09edac74a1dde5d97676fb9f1c3539e6c9591
Luan Magno <81940672+LuanMagno@users.noreply.github.com>
2023-10-18 14:36:00 -0300

    Add files via upload

---
+++ DespesasRestaurante.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RestauranteGorjetaePagamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double ValorGasto, Gorjeta,ValorF;
+            Console.WriteLine("Insira o valor gasto com as despesas do restaurante: ");
+            ValorGasto = (double.Parse(Console.ReadLine()));
+            Console.WriteLine("Insira o percentual da gorjeta: ");
+            Gorjeta = (double.Parse(Console.ReadLine()));
+            Gorjeta = (Gorjeta * ValorGasto) / 100;
+            ValorF = ValorGasto + Gorjeta;
+            Console.WriteLine("O valor da gorjeta será de " + Gorjeta + ", e o valor final será de " + ValorF);
+        }
+    }
+}
