faf3bc567957e933f574cfddc30870801a6f250e
Luan Magno <81940672+LuanMagno@users.noreply.github.com>
2023-10-29 10:06:33 -0300

    Add files via upload

---
+++ MenorMaiorIdade.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MenorIdade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade1, idade2, idade3,menorId,maiorId;
+            Console.WriteLine("Insira a primeira idade: ");
+            idade1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Insira a segunda idade: ");
+            idade2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Insira a terceira idade: ");
+            idade3 = int.Parse(Console.ReadLine());
+            if (idade1 >= idade2 & idade1 >= idade3)
+            {
+                maiorId = idade1;
+            }
+            else if (idade2 >= idade1 & idade2 >= idade3){
+                maiorId = idade2;
+            }
+            else
+            {
+                maiorId = idade3;
+
+            }
+            if (idade1 <= idade2 & idade1 <= idade3)
+            {
+                menorId= idade1;
+            }
+            else if (idade2 <= idade1 & idade2 <= idade3)
+            {
+                menorId = idade2;
+            }
+            else
+            {
+                menorId = idade3;
+
+            }
+            Console.WriteLine("A maior idade é:" + maiorId + "\nA menor idade é:" + menorId);
+
+        }
+    }
+}
