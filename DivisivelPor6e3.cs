5146564040149de03bedf1e0b4182541a33f77d2
Luan Magno <81940672+LuanMagno@users.noreply.github.com>
2023-10-29 10:07:43 -0300

    Add files via upload

---
+++ DivisivelPor6e3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DivisivelPor3e6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numb;
+            Console.WriteLine("Digite um número: ");
+            numb = int.Parse(Console.ReadLine());
+            if(numb%3 == 0 & numb%6==0) {
+                Console.WriteLine(numb + " é divisível por 3 e por 6");  
+            }
+            else
+            {
+                Console.WriteLine("Não é divisível por 3 e por 6 simultaneamente");
+            }
+        }
+    }
+}
