dbd63ec82742e03222f50c6155f6107adff1828a
Luan Magno <81940672+LuanMagno@users.noreply.github.com>
2023-10-29 10:08:32 -0300

    Add files via upload

---
+++ BrindePosto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PostoGasolina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double litros;
+            string brinde;
+            Console.WriteLine("Quantos litros foram colocados: ");
+            litros = double.Parse(Console.ReadLine());
+            brinde = (litros < 10) ? "chaveiro" : "ducha";
+            Console.WriteLine("Seu brinde é: " + brinde);
+        }
+    }
+}
