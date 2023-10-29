e81f3b7cf9e14f414c19ccb0f4ce3c5d0c0ca3d3
Luan Magno <81940672+LuanMagno@users.noreply.github.com>
2023-10-29 10:07:09 -0300

    Add files via upload

---
+++ CNHtipoD.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CNHtipoD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade, tempocarteira;
+            char tipoCNH;
+            Console.WriteLine("Insira sua idade: ");
+            idade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Insira sua carteira(B ou C): ");
+            tipoCNH = char.Parse(Console.ReadLine());
+            Console.WriteLine("A quantos anos você tem a carteira: ");
+            tempocarteira = int.Parse(Console.ReadLine());
+            if(idade>=21 & (tipoCNH == 'B' & tempocarteira>=2 || tipoCNH =='C' & tempocarteira>=1)) {
+                Console.WriteLine("Você está apto a tirar carteira do tipo D");
+            }
+            else if(idade < 21 & (tipoCNH == 'B' & tempocarteira >= 2 || tipoCNH == 'C' & tempocarteira >= 1))
+            {
+                Console.WriteLine("Você possui o tempo de carteira necessário mas não possui a idade");
+            }
+            else if(idade >= 21 & (tipoCNH == 'B' & tempocarteira < 2 || tipoCNH == 'C' & tempocarteira < 1))
+            {
+                Console.WriteLine("Você possui a idade necessária mas não atende ao tempo de carteira necessário");
+            }
+            else
+            {
+                Console.WriteLine("Você não atende a nenhum dos requisitos");
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
