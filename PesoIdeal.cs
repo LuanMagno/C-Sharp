09b94d03058861f26c8b42c7a257777a5119d36a
Luan Magno <81940672+LuanMagno@users.noreply.github.com>
2023-10-29 10:08:47 -0300

    Add files via upload

---
+++ PesoIdeal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PesoIdeal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char sexo;
+            double altura, peso;
+            Console.WriteLine("Insira sua altura: ");
+            altura = double.Parse(Console.ReadLine());
+            Console.WriteLine("Insira seu sexo biológico(M/F): ");
+            sexo = char.Parse(Console.ReadLine());
+            peso = (sexo == 'M') ? (72.7 * altura) - 58 :(62.1 * altura) - 44.7;
+            Console.WriteLine($"Seu peso ideal é {peso}");
+        }
+    }
+}
